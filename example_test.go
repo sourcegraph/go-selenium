9f260365183da6a92ebe3515ba95e0f4a2525c95
Quinn Slack <qslack@qslack.com>
2013-06-01 00:06:43 -0700

    Add improved README with examples

---
+++ example_test.go
@@ -0,0 +1,48 @@
+package selenium_test
+
+import (
+	"fmt"
+	"github.com/sqs/go-selenium"
+)
+
+func ExampleFindElement() {
+	var webDriver selenium.WebDriver
+	var err error
+	caps := selenium.Capabilities(map[string]interface{}{"browserName": "firefox"})
+	if webDriver, err = selenium.NewRemote(caps, "http://localhost:4444/wd/hub"); err != nil {
+		fmt.Printf("Failed to open session: %s\n", err)
+		return
+	}
+	defer webDriver.Quit()
+
+	err = webDriver.Get("https://github.com/sqs/go-selenium")
+	if err != nil {
+		fmt.Printf("Failed to load page: %s\n", err)
+		return
+	}
+
+	if title, err := webDriver.Title(); err == nil {
+		fmt.Printf("Page title: %s\n", title)
+	} else {
+		fmt.Printf("Failed to get page title: %s", err)
+		return
+	}
+
+	var elem selenium.WebElement
+	elem, err = webDriver.FindElement(selenium.ByCSSSelector, ".js-current-repository")
+	if err != nil {
+		fmt.Printf("Failed to find element: %s\n", err)
+		return
+	}
+
+	if text, err := elem.Text(); err == nil {
+		fmt.Printf("Repository: %s\n", text)
+	} else {
+		fmt.Printf("Failed to get text of element: %s\n", err)
+		return
+	}
+
+	// output:
+	// Page title: sqs/go-selenium · GitHub
+	// Repository: go-selenium
+}
