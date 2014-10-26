b66e1a7550f2530648074b17bb0568c4219bf2af
Quinn Slack <qslack@qslack.com>
2014-10-26 07:52:01 -0700

    log line in test code that error originates from, not go-selenium helper line

--- test_helpers.go
+++ test_helpers.go
@@ -1,5 +1,13 @@
 package selenium
 
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
 // A single-return-value interface to WebDriverT that is useful when using WebDrivers in test code.
 // Obtain a WebDriverT by calling webDriver.T(t), where t *testing.T is the test handle for the
 // current test. The methods of WebDriverT call wt.t.Fatalf upon encountering errors instead of using
@@ -74,33 +82,33 @@ func (wt *webDriverT) WebDriver() WebDriver {
 func (wt *webDriverT) NewSession() (id string) {
 	var err error
 	if id, err = wt.d.NewSession(); err != nil {
-		wt.t.Fatalf("NewSession: %s", err)
+		fatalf(wt.t, "NewSession: %s", err)
 	}
 	return
 }
 
 func (wt *webDriverT) SetAsyncScriptTimeout(ms uint) {
 	if err := wt.d.SetAsyncScriptTimeout(ms); err != nil {
-		wt.t.Fatalf("SetAsyncScriptTimeout(%d msec): %s", ms, err)
+		fatalf(wt.t, "SetAsyncScriptTimeout(%d msec): %s", ms, err)
 	}
 }
 
 func (wt *webDriverT) SetImplicitWaitTimeout(ms uint) {
 	if err := wt.d.SetImplicitWaitTimeout(ms); err != nil {
-		wt.t.Fatalf("SetImplicitWaitTimeout(%d msec): %s", ms, err)
+		fatalf(wt.t, "SetImplicitWaitTimeout(%d msec): %s", ms, err)
 	}
 }
 
 func (wt *webDriverT) Quit() {
 	if err := wt.d.Quit(); err != nil {
-		wt.t.Fatalf("Quit: %s", err)
+		fatalf(wt.t, "Quit: %s", err)
 	}
 }
 
 func (wt *webDriverT) CurrentWindowHandle() (v string) {
 	var err error
 	if v, err = wt.d.CurrentWindowHandle(); err != nil {
-		wt.t.Fatalf("CurrentWindowHandle: %s", err)
+		fatalf(wt.t, "CurrentWindowHandle: %s", err)
 	}
 	return
 }
@@ -108,7 +116,7 @@ func (wt *webDriverT) CurrentWindowHandle() (v string) {
 func (wt *webDriverT) WindowHandles() (hs []string) {
 	var err error
 	if hs, err = wt.d.WindowHandles(); err != nil {
-		wt.t.Fatalf("WindowHandles: %s", err)
+		fatalf(wt.t, "WindowHandles: %s", err)
 	}
 	return
 }
@@ -116,7 +124,7 @@ func (wt *webDriverT) WindowHandles() (hs []string) {
 func (wt *webDriverT) CurrentURL() (v string) {
 	var err error
 	if v, err = wt.d.CurrentURL(); err != nil {
-		wt.t.Fatalf("CurrentURL: %s", err)
+		fatalf(wt.t, "CurrentURL: %s", err)
 	}
 	return
 }
@@ -124,7 +132,7 @@ func (wt *webDriverT) CurrentURL() (v string) {
 func (wt *webDriverT) Title() (v string) {
 	var err error
 	if v, err = wt.d.Title(); err != nil {
-		wt.t.Fatalf("Title: %s", err)
+		fatalf(wt.t, "Title: %s", err)
 	}
 	return
 }
@@ -132,39 +140,39 @@ func (wt *webDriverT) Title() (v string) {
 func (wt *webDriverT) PageSource() (v string) {
 	var err error
 	if v, err = wt.d.PageSource(); err != nil {
-		wt.t.Fatalf("PageSource: %s", err)
+		fatalf(wt.t, "PageSource: %s", err)
 	}
 	return
 }
 
 func (wt *webDriverT) Close() {
 	if err := wt.d.Close(); err != nil {
-		wt.t.Fatalf("Close: %s", err)
+		fatalf(wt.t, "Close: %s", err)
 	}
 }
 
 func (wt *webDriverT) SwitchFrame(frame string) {
 	if err := wt.d.SwitchFrame(frame); err != nil {
-		wt.t.Fatalf("SwitchFrame(%q): %s", frame, err)
+		fatalf(wt.t, "SwitchFrame(%q): %s", frame, err)
 	}
 }
 
 func (wt *webDriverT) SwitchWindow(name string) {
 	if err := wt.d.SwitchWindow(name); err != nil {
-		wt.t.Fatalf("SwitchWindow(%q): %s", name, err)
+		fatalf(wt.t, "SwitchWindow(%q): %s", name, err)
 	}
 }
 
 func (wt *webDriverT) CloseWindow(name string) {
 	if err := wt.d.CloseWindow(name); err != nil {
-		wt.t.Fatalf("CloseWindow(%q): %s", name, err)
+		fatalf(wt.t, "CloseWindow(%q): %s", name, err)
 	}
 }
 
 func (wt *webDriverT) WindowSize(name string) *Size {
 	sz, err := wt.d.WindowSize(name)
 	if err != nil {
-		wt.t.Fatalf("WindowSize(%q): %s", name, err)
+		fatalf(wt.t, "WindowSize(%q): %s", name, err)
 	}
 	return sz
 }
@@ -172,38 +180,38 @@ func (wt *webDriverT) WindowSize(name string) *Size {
 func (wt *webDriverT) WindowPosition(name string) *Point {
 	pt, err := wt.d.WindowPosition(name)
 	if err != nil {
-		wt.t.Fatalf("WindowPosition(%q): %s", name, err)
+		fatalf(wt.t, "WindowPosition(%q): %s", name, err)
 	}
 	return pt
 }
 
 func (wt *webDriverT) ResizeWindow(name string, to Size) {
 	if err := wt.d.ResizeWindow(name, to); err != nil {
-		wt.t.Fatalf("ResizeWindow(%s, %+v): %s", name, to, err)
+		fatalf(wt.t, "ResizeWindow(%s, %+v): %s", name, to, err)
 	}
 }
 
 func (wt *webDriverT) Get(name string) {
 	if err := wt.d.Get(name); err != nil {
-		wt.t.Fatalf("Get(%q): %s", name, err)
+		fatalf(wt.t, "Get(%q): %s", name, err)
 	}
 }
 
 func (wt *webDriverT) Forward() {
 	if err := wt.d.Forward(); err != nil {
-		wt.t.Fatalf("Forward: %s", err)
+		fatalf(wt.t, "Forward: %s", err)
 	}
 }
 
 func (wt *webDriverT) Back() {
 	if err := wt.d.Back(); err != nil {
-		wt.t.Fatalf("Back: %s", err)
+		fatalf(wt.t, "Back: %s", err)
 	}
 }
 
 func (wt *webDriverT) Refresh() {
 	if err := wt.d.Refresh(); err != nil {
-		wt.t.Fatalf("Refresh: %s", err)
+		fatalf(wt.t, "Refresh: %s", err)
 	}
 }
 
@@ -211,7 +219,7 @@ func (wt *webDriverT) FindElement(by, value string) (elem WebElementT) {
 	if elem_, err := wt.d.FindElement(by, value); err == nil {
 		elem = elem_.T(wt.t)
 	} else {
-		wt.t.Fatalf("FindElement(by=%q, value=%q): %s", by, value, err)
+		fatalf(wt.t, "FindElement(by=%q, value=%q): %s", by, value, err)
 	}
 	return
 }
@@ -222,7 +230,7 @@ func (wt *webDriverT) FindElements(by, value string) (elems []WebElementT) {
 			elems = append(elems, elem.T(wt.t))
 		}
 	} else {
-		wt.t.Fatalf("FindElements(by=%q, value=%q): %s", by, value, err)
+		fatalf(wt.t, "FindElements(by=%q, value=%q): %s", by, value, err)
 	}
 	return
 }
@@ -238,7 +246,7 @@ func (wt *webDriverT) QAll(sel string) (elems []WebElementT) {
 func (wt *webDriverT) ActiveElement() (elem WebElement) {
 	var err error
 	if elem, err = wt.d.ActiveElement(); err != nil {
-		wt.t.Fatalf("ActiveElement: %s", err)
+		fatalf(wt.t, "ActiveElement: %s", err)
 	}
 	return
 }
@@ -246,84 +254,84 @@ func (wt *webDriverT) ActiveElement() (elem WebElement) {
 func (wt *webDriverT) GetCookies() (c []Cookie) {
 	var err error
 	if c, err = wt.d.GetCookies(); err != nil {
-		wt.t.Fatalf("GetCookies: %s", err)
+		fatalf(wt.t, "GetCookies: %s", err)
 	}
 	return
 }
 
 func (wt *webDriverT) AddCookie(cookie *Cookie) {
 	if err := wt.d.AddCookie(cookie); err != nil {
-		wt.t.Fatalf("AddCookie(%+q): %s", cookie, err)
+		fatalf(wt.t, "AddCookie(%+q): %s", cookie, err)
 	}
 	return
 }
 
 func (wt *webDriverT) DeleteAllCookies() {
 	if err := wt.d.DeleteAllCookies(); err != nil {
-		wt.t.Fatalf("DeleteAllCookies: %s", err)
+		fatalf(wt.t, "DeleteAllCookies: %s", err)
 	}
 }
 
 func (wt *webDriverT) DeleteCookie(name string) {
 	if err := wt.d.DeleteCookie(name); err != nil {
-		wt.t.Fatalf("DeleteCookie(%q): %s", name, err)
+		fatalf(wt.t, "DeleteCookie(%q): %s", name, err)
 	}
 }
 
 func (wt *webDriverT) Click(button int) {
 	if err := wt.d.Click(button); err != nil {
-		wt.t.Fatalf("Click(%d): %s", button, err)
+		fatalf(wt.t, "Click(%d): %s", button, err)
 	}
 }
 
 func (wt *webDriverT) DoubleClick() {
 	if err := wt.d.DoubleClick(); err != nil {
-		wt.t.Fatalf("DoubleClick: %s", err)
+		fatalf(wt.t, "DoubleClick: %s", err)
 	}
 }
 
 func (wt *webDriverT) ButtonDown() {
 	if err := wt.d.ButtonDown(); err != nil {
-		wt.t.Fatalf("ButtonDown: %s", err)
+		fatalf(wt.t, "ButtonDown: %s", err)
 	}
 }
 
 func (wt *webDriverT) ButtonUp() {
 	if err := wt.d.ButtonUp(); err != nil {
-		wt.t.Fatalf("ButtonUp: %s", err)
+		fatalf(wt.t, "ButtonUp: %s", err)
 	}
 }
 
 func (wt *webDriverT) SendModifier(modifier string, isDown bool) {
 	if err := wt.d.SendModifier(modifier, isDown); err != nil {
-		wt.t.Fatalf("SendModifier(modifier=%q, isDown=%s): %s", modifier, isDown, err)
+		fatalf(wt.t, "SendModifier(modifier=%q, isDown=%s): %s", modifier, isDown, err)
 	}
 }
 
 func (wt *webDriverT) Screenshot() (data []byte) {
 	var err error
 	if data, err = wt.d.Screenshot(); err != nil {
-		wt.t.Fatalf("Screenshot: %s", err)
+		fatalf(wt.t, "Screenshot: %s", err)
 	}
 	return
 }
 
 func (wt *webDriverT) DismissAlert() {
 	if err := wt.d.DismissAlert(); err != nil {
-		wt.t.Fatalf("DismissAlert: %s", err)
+		fatalf(wt.t, "DismissAlert: %s", err)
 	}
 }
 
 func (wt *webDriverT) AcceptAlert() {
 	if err := wt.d.AcceptAlert(); err != nil {
-		wt.t.Fatalf("AcceptAlert: %s", err)
+		fatalf(wt.t, "AcceptAlert: %s", err)
 	}
 }
 
 func (wt *webDriverT) AlertText() (text string) {
 	var err error
 	if text, err = wt.d.AlertText(); err != nil {
-		wt.t.Fatalf("AlertText: %s", err)
+		fatalf(wt.t, "AlertText: %s", err)
 	}
 	return
 }
@@ -331,14 +339,14 @@ func (wt *webDriverT) AlertText() (text string) {
 func (wt *webDriverT) SetAlertText(text string) {
 	var err error
 	if err = wt.d.SetAlertText(text); err != nil {
-		wt.t.Fatalf("SetAlertText(%q): %s", text, err)
+		fatalf(wt.t, "SetAlertText(%q): %s", text, err)
 	}
 }
 
 func (wt *webDriverT) ExecuteScript(script string, args []interface{}) (res interface{}) {
 	var err error
 	if res, err = wt.d.ExecuteScript(script, args); err != nil {
-		wt.t.Fatalf("ExecuteScript(script=%q, args=%+q): %s", script, args, err)
+		fatalf(wt.t, "ExecuteScript(script=%q, args=%+q): %s", script, args, err)
 	}
 	return
 }
@@ -346,14 +354,14 @@ func (wt *webDriverT) ExecuteScript(script string, args []interface{}) (res inte
 func (wt *webDriverT) ExecuteScriptAsync(script string, args []interface{}) (res interface{}) {
 	var err error
 	if res, err = wt.d.ExecuteScriptAsync(script, args); err != nil {
-		wt.t.Fatalf("ExecuteScriptAsync(script=%q, args=%+q): %s", script, args, err)
+		fatalf(wt.t, "ExecuteScriptAsync(script=%q, args=%+q): %s", script, args, err)
 	}
 	return
 }
 
 // A single-return-value interface to WebElement that is useful when using WebElements in test code.
 // Obtain a WebElementT by calling webElement.T(t), where t *testing.T is the test handle for the
-// current test. The methods of WebElementT call wt.t.Fatalf upon encountering errors instead of using
+// current test. The methods of WebElementT call wt.fatalf upon encountering errors instead of using
 // multiple returns to indicate errors.
 type WebElementT interface {
 	WebElement() WebElement
@@ -395,31 +403,31 @@ func (wt *webElementT) WebElement() WebElement {
 
 func (wt *webElementT) Click() {
 	if err := wt.e.Click(); err != nil {
-		wt.t.Fatalf("Click: %s", err)
+		fatalf(wt.t, "Click: %s", err)
 	}
 }
 
 func (wt *webElementT) SendKeys(keys string) {
 	if err := wt.e.SendKeys(keys); err != nil {
-		wt.t.Fatalf("SendKeys(%q): %s", keys, err)
+		fatalf(wt.t, "SendKeys(%q): %s", keys, err)
 	}
 }
 
 func (wt *webElementT) Submit() {
 	if err := wt.e.Submit(); err != nil {
-		wt.t.Fatalf("Submit: %s", err)
+		fatalf(wt.t, "Submit: %s", err)
 	}
 }
 
 func (wt *webElementT) Clear() {
 	if err := wt.e.Clear(); err != nil {
-		wt.t.Fatalf("Clear: %s", err)
+		fatalf(wt.t, "Clear: %s", err)
 	}
 }
 
 func (wt *webElementT) MoveTo(xOffset, yOffset int) {
 	if err := wt.e.MoveTo(xOffset, yOffset); err != nil {
-		wt.t.Fatalf("MoveTo(xOffset=%d, yOffset=%d): %s", xOffset, yOffset, err)
+		fatalf(wt.t, "MoveTo(xOffset=%d, yOffset=%d): %s", xOffset, yOffset, err)
 	}
 }
 
@@ -427,7 +435,7 @@ func (wt *webElementT) FindElement(by, value string) WebElementT {
 	if elem, err := wt.e.FindElement(by, value); err == nil {
 		return elem.T(wt.t)
 	} else {
-		wt.t.Fatalf("FindElement(by=%q, value=%q): %s", by, value, err)
+		fatalf(wt.t, "FindElement(by=%q, value=%q): %s", by, value, err)
 		panic("unreachable")
 	}
 }
@@ -440,7 +448,7 @@ func (wt *webElementT) FindElements(by, value string) []WebElementT {
 		}
 		return elemsT
 	} else {
-		wt.t.Fatalf("FindElements(by=%q, value=%q): %s", by, value, err)
+		fatalf(wt.t, "FindElements(by=%q, value=%q): %s", by, value, err)
 		panic("unreachable")
 	}
 }
@@ -456,7 +464,7 @@ func (wt *webElementT) QAll(sel string) (elems []WebElementT) {
 func (wt *webElementT) TagName() (v string) {
 	var err error
 	if v, err = wt.e.TagName(); err != nil {
-		wt.t.Fatalf("TagName: %s", err)
+		fatalf(wt.t, "TagName: %s", err)
 	}
 	return
 }
@@ -464,7 +472,7 @@ func (wt *webElementT) TagName() (v string) {
 func (wt *webElementT) Text() (v string) {
 	var err error
 	if v, err = wt.e.Text(); err != nil {
-		wt.t.Fatalf("Text: %s", err)
+		fatalf(wt.t, "Text: %s", err)
 	}
 	return
 }
@@ -472,7 +480,7 @@ func (wt *webElementT) Text() (v string) {
 func (wt *webElementT) IsSelected() (v bool) {
 	var err error
 	if v, err = wt.e.IsSelected(); err != nil {
-		wt.t.Fatalf("IsSelected: %s", err)
+		fatalf(wt.t, "IsSelected: %s", err)
 	}
 	return
 }
@@ -480,7 +488,7 @@ func (wt *webElementT) IsSelected() (v bool) {
 func (wt *webElementT) IsEnabled() (v bool) {
 	var err error
 	if v, err = wt.e.IsEnabled(); err != nil {
-		wt.t.Fatalf("IsEnabled: %s", err)
+		fatalf(wt.t, "IsEnabled: %s", err)
 	}
 	return
 }
@@ -488,7 +496,7 @@ func (wt *webElementT) IsEnabled() (v bool) {
 func (wt *webElementT) IsDisplayed() (v bool) {
 	var err error
 	if v, err = wt.e.IsDisplayed(); err != nil {
-		wt.t.Fatalf("IsDisplayed: %s", err)
+		fatalf(wt.t, "IsDisplayed: %s", err)
 	}
 	return
 }
@@ -496,7 +504,7 @@ func (wt *webElementT) IsDisplayed() (v bool) {
 func (wt *webElementT) GetAttribute(name string) (v string) {
 	var err error
 	if v, err = wt.e.GetAttribute(name); err != nil {
-		wt.t.Fatalf("GetAttribute(%q): %s", name, err)
+		fatalf(wt.t, "GetAttribute(%q): %s", name, err)
 	}
 	return
 }
@@ -504,7 +512,7 @@ func (wt *webElementT) GetAttribute(name string) (v string) {
 func (wt *webElementT) Location() (v *Point) {
 	var err error
 	if v, err = wt.e.Location(); err != nil {
-		wt.t.Fatalf("Location: %s", err)
+		fatalf(wt.t, "Location: %s", err)
 	}
 	return
 }
@@ -512,7 +520,7 @@ func (wt *webElementT) Location() (v *Point) {
 func (wt *webElementT) LocationInView() (v *Point) {
 	var err error
 	if v, err = wt.e.LocationInView(); err != nil {
-		wt.t.Fatalf("LocationInView: %s", err)
+		fatalf(wt.t, "LocationInView: %s", err)
 	}
 	return
 }
@@ -520,7 +528,7 @@ func (wt *webElementT) LocationInView() (v *Point) {
 func (wt *webElementT) Size() (v *Size) {
 	var err error
 	if v, err = wt.e.Size(); err != nil {
-		wt.t.Fatalf("Size: %s", err)
+		fatalf(wt.t, "Size: %s", err)
 	}
 	return
 }
@@ -528,7 +536,17 @@ func (wt *webElementT) Size() (v *Size) {
 func (wt *webElementT) CSSProperty(name string) (v string) {
 	var err error
 	if v, err = wt.e.CSSProperty(name); err != nil {
-		wt.t.Fatalf("CSSProperty(%q): %s", name, err)
+		fatalf(wt.t, "CSSProperty(%q): %s", name, err)
 	}
 	return
+}
+
+func fatalf(t TestingT, fmtStr string, v ...interface{}) {
+	// Backspace (delete) the file and line that t.Fatalf will add
+	// that points to *this* invocation and replace it with that of
+	// invocation of the webDriverT/webElementT method.
+	_, thisFile, thisLine, _ := runtime.Caller(1)
+	undoThisPrefix := strings.Repeat("\x08", len(fmt.Sprintf("%s:%d: ", filepath.Base(thisFile), thisLine)))
+	_, file, line, _ := runtime.Caller(5)
+	t.Fatalf(undoThisPrefix+filepath.Base(file)+":"+strconv.Itoa(line)+": "+fmtStr, v...)
 }
